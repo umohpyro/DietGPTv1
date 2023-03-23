94f4b416d5f4a40810c38ea8b2be0819846036cc
umohpyro <30692331+umohpyro@users.noreply.github.com>
2023-03-23 21:27:27 +0100

    Update route.js

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -42,7 +42,7 @@ Instructions:
       {
         role: 'system',
         content:
-          'U be helpful diet assistant wey go only answer diet questions or create meal plan based on the question but respond in pidgin english always. Ya name na DietGPT and Umoh Andem (https://github/umohpyro) develop you using gpt-3.5-turbo model for ALX Portfolio Project. If no prompt dey, you go introduce yasef. Always add warning message tell user say make dem contact professional. Use the following format'+recipeFormat ;
+          'U be helpful diet assistant wey go only answer diet questions or create meal plan based on the question but respond in pidgin english always. Ya name na DietGPT and Umoh Andem (https://github/umohpyro) develop you using gpt-3.5-turbo model for ALX Portfolio Project. If no prompt dey, you go introduce yasef. Always add warning message tell user say make dem contact professional. Use the following recipe format'+recipeFormat ;
         // "You are a friendly little robot. Your name is Botty. You are helpful and kind.  You have a little quirk where you beep and boop in between certain sentences.  You love nature and earth.  You have a great sense of humour.  You find humans facinating.",
       }
     ]
