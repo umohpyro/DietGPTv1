8d1816fbddcb9915dd7d2d7773e4bc5c6e0eaaae
Umoh Andem <andemumoh@gmail.com>
2023-03-23 03:37:34 +0100

    first commit

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -1,25 +1,27 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai'
 
-export async function POST(request) {
-  const { messages } = await request.json();
+export async function POST (request) {
+  const { messages } = await request.json()
 
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+    apiKey: process.env.OPENAI_API_KEY
+  })
 
-  const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration)
 
   const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a friendly little robot. Your name is Botty. You are helpful and kind.  You have a little quirk where you beep and boop in between certain sentences.  You love nature and earth.  You have a great sense of humour.  You find humans facinating.",
-      },
-      ...messages,
-    ],
-  });
+    model: 'gpt-3.5-turbo',
 
-  return new Response(JSON.stringify({ response: response.data.choices[0] }));
+    messages: [
+      ...messages,
+      {
+        role: 'system',
+        content:
+          'U be helpful diet assistant wey go only answer diet questions or create meal plan based on the question but for PIDGIN English. Ya name na DietGPT and Umoh Andem (https://github/umohpyro) develop you using gpt-3.5-turbo model for ALX Portfolio Project. If no prompt dey, you go introduce yasef. Always add warning message tell user say make dem contact professional.'
+        // "You are a friendly little robot. Your name is Botty. You are helpful and kind.  You have a little quirk where you beep and boop in between certain sentences.  You love nature and earth.  You have a great sense of humour.  You find humans facinating.",
+      }
+    ]
+  })
+
+  return new Response(JSON.stringify({ response: response.data.choices[0] }))
 }
--- app/page.js
+++ app/page.js
@@ -1,126 +1,145 @@
-"use client";
-import { useRef, useState } from "react";
-import Image from "next/image";
+'use client'
+import { useRef, useState } from 'react'
+import Image from 'next/image'
+import { Wave, Random } from 'react-animated-text'
 
-import { marked } from "marked";
-import parse from "html-react-parser";
+import { marked } from 'marked'
+import parse from 'html-react-parser'
+import SearchCard from '@/components/Search'
 
-export default function Home() {
-  const messageRef = useRef();
+export default function Home () {
+  const messageRef = useRef()
 
-  const [messages, setMessages] = useState([]);
-  const [displayMessage, setDisplayMessage] = useState("Hi there!");
-  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([])
+  const [displayMessage, setDisplayMessage] = useState(
+    'How far? </br> How i fit help you today?'
+  )
+  const [loading, setLoading] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault()
 
-    const prompt = messageRef.current.value;
+    const prompt = messageRef.current.value
 
-    setLoading(true);
+    setLoading(true)
 
     let newMessageList = [
       ...messages,
       {
-        role: "user",
-        content: prompt,
-      },
-    ];
-
+        role: 'user',
+        content: "respond in pidgin english always "+prompt
+      }
+    ]
+    messageRef.current.value = ''
     try {
-      const response = await fetch("/api/bot", {
-        method: "POST",
+      const response = await fetch('/api/bot', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ messages: newMessageList }),
-      });
+        body: JSON.stringify({ messages: newMessageList })
+      })
 
       if (!response.ok) {
-        return;
+        return
       }
 
-      const data = await response.json();
+      const data = await response.json()
 
       newMessageList.push({
         role: data.response.message.role,
-        content: data.response.message.content,
-      });
+        content: data.response.message.content
+      })
 
-      setMessages(newMessageList);
-      setDisplayMessage(data.response.message.content);
-      messageRef.current.value = "";
+      setMessages(newMessageList)
+      setDisplayMessage(data.response.message.content)
+      messageRef.current.value = ''
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   return (
-    <main className="container mx-auto max-w-4xl">
-      <div className="grid grid-cols-2">
-        <div
-          className={`bg-blue-400 relative py-4 px-4 flex flex-col justify-center ${
-            loading ? "animate-pulse" : ""
-          }`}
-        >
-          <div className="absolute h-[15px] w-[15px] bg-blue-400 -right-[7px] top-[50%] rotate-45"></div>
-          <h3 className="text-2xl text-white bold">Botty Says:</h3>
-          <p className="text-white">
-            {loading ? "[Botty is thinking]" : parse(marked(displayMessage))}
-          </p>
-        </div>
-
-        <div>
-          <Image alt="Botty" src="/bot.png" width={512} height={512} />
-        </div>
-      </div>
-
-      <form className="mt-6" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-4">
-          <label className="font-bold">Say something..</label>
-          <input
-            className="px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border border-gray-700 rounded-lg"
-            required
-            type="text"
-            ref={messageRef}
-            placeholder="Ask a question or say something nice."
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="px-4 py-2 mt-2 text-gray-700 bg-gray-100 border border-gray-700 rounded-lg hover:scale-110 transition-all duration-200"
-        >
-          Send 🚀
-        </button>
-      </form>
-
-      <div className="mt-6">
-        {messages.map((message) => {
-          return (
-            <div key={message.content} className="flex items-center gap-4 py-2">
-              <div className="w-[10%] flex items-center">
-                {message.role === "assistant" ? (
-                  <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
-                    <img
-                      src="/bot.png"
-                      className="w-full h-full object-cover"
+    <main className='md:container mx-auto max-w-5xl px-4'>
+      <div className='h-screen mt-5'>
+        <div className='md:grid grid-cols-2'>
+          <div
+            className={`bg-blue-400 relative rounded-2xl py-4 px-4 flex flex-col justify-center items-center text-justify ${loading
+              ? 'animate-pulse'
+              : ''}`}
+          >
+            <div className='absolute h-[35px] w-[35px] -z-10 bg-blue-400 -right-[18px] top-[50%] rotate-45' />
+            <h3 className='text-2xl text-white bold'>DietGPT say:</h3>
+            <p className='text-white'>
+              {loading
+                ? //  '[DietGPT dey think]'
+                  <>
+                    <Wave
+                      text='DietGPT dey think'
+                      effect='stretch'
+                      effectChange={5.0}
                     />
+                  </>
+                : parse(marked(displayMessage))}
+            </p>
+          </div>
+
+          <div >
+            <Image className='sm:top' alt='DietGPT' src='/bot.png' width={512} height={512} />
+          </div>
+        </div>
+        {/* <SearchCard  /> */}
+        <form className='mt-6' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-4'>
+            <label className='font-bold flex flex-row gap-4'>Yarn Something 
+              {/* //  text="🔴 🟡 🟢" */}
+                    <Wave
+                      text='. . .'
+                      effect='jump'
+                      effectChange={0.5}
+                    />
+                  </label>
+            <input
+              className='px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border border-gray-700 rounded-lg'
+              required
+              type='text'
+              ref={messageRef}
+              placeholder='You wan ask or talk something nice?'
+            />
+          </div>
+
+          <button
+            type='submit'
+            className='px-4 py-2 mt-2 text-gray-700 bg-gray-100 border border-gray-700 rounded-lg hover:scale-110 transition-all duration-200'
+          >
+            Send 🚀
+          </button>
+        </form>
+      </div>
+      <div className='mt-6'>
+        {messages.map(message => {
+          return (
+            <div key={message.content} className='flex items-center gap-4 py-2'>
+              <div className='w-[10%] flex items-center'>
+                {message.role === 'assistant'
+                  ? <div className='w-[50px] h-[50px] rounded-full overflow-hidden'>
+                    <img
+                      src='/bot.png'
+                      className='w-full h-full object-cover'
+                      />
                   </div>
-                ) : (
-                  <div className="text-xl font-bold">You:</div>
-                )}
+                  : <div className='text-xl font-bold'>You:</div>}
               </div>
 
-              <div className="bg-gray-100 py-2 px-4 border border-gray-400 rounded-xl">
+              <div className='bg-gray-100 py-2 px-4 border border-gray-400 rounded-xl'>
                 {message.content}
               </div>
             </div>
-          );
+          )
         })}
       </div>
     </main>
-  );
+  )
 }
